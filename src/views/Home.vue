343d53d524d79eae2f71a20f26cb6e6ea6aa51f1
MarinesLemus007 <marineslemus@gmail.com>
2021-03-14 20:13:24 -0300

    diseño en vista Home y Location

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,46 @@
 <template>
-  <h1>Home</h1>
+    <div>
+
+    <v-container>
+        <v-row class="mt-5">
+            <v-col>
+                <div class="d-flex justify-center">
+                    <img
+                    style="max-width: 320px; height: auto;"
+                    src="../assets/img_rickandmorty_text.png"
+                    >
+                
+                    <img
+                    style="max-width: 320px; height: auto;"
+                    src="../assets/img_rickandmorty.png"
+                    >
+                </div>
+            </v-col>
+        </v-row>
+
+        <v-row>
+            <v-col>
+                <div class="d-flex align-center justify-center">
+                    <h1 class="white--text">See you all:</h1>
+
+                    <v-btn class="mx-2" to="/Characters" color="pink" dark small>
+                        Characters
+                    </v-btn>
+
+                    <v-btn class="mr-2" to="/Locations" color="pink" dark small>
+                        Locations
+                    </v-btn>
+
+                    <v-btn to="/Episodes" color="pink" dark small>
+                        Episodes
+                    </v-btn>
+                </div>
+
+            </v-col>
+        </v-row>
+    </v-container>
+
+    </div>
 </template>
 
 <script>
--- src/views/Locations.vue
+++ src/views/Locations.vue
@@ -1,9 +1,87 @@
 <template>
-  <h1>Locations</h1>
+
+    <v-container>
+      <v-row class="justify-center mt-5"  dense>
+        <v-col v-for="(locations, index) in arrayLocations" :key="index" cols="12" sm="6" md="6" lg="4">
+          <v-card
+            color="#385F73"
+            dark
+          >
+            <v-card-title>
+             <h5>{{locations.dimension}}</h5>
+            </v-card-title>
+
+            <v-card-subtitle>
+                <ul style="font-size: 0.9em">
+                    <li>Name: {{locations.name}}</li>
+                    <li>Residents: {{locations.residents.length}}</li>
+                    <li>Type: {{locations.type}}</li>
+                </ul>
+           </v-card-subtitle>
+
+          </v-card>
+        </v-col>
+
+        <v-col cols="12" class="d-flex justify-center align-center">
+            <v-btn v-if="numPage > 1" @click="getCharacter(numPage = numPage - 1)" text color="orange">
+            <v-icon
+            small
+            left
+            color="orange"
+            >
+                mdi-chevron-left
+            </v-icon>
+            previous
+            </v-btn>
+
+        <div class="px-5 py-3 mx-3 secondary rounded-circle d-inline-block white--text">
+          {{numPage}}
+        </div>
+
+        <v-btn v-if="numPage <= 5" @click="getCharacter(numPage = numPage + 1)" text color="orange">
+          Next
+          <v-icon
+          small
+          right
+          color="orange"
+          >
+            mdi-chevron-right
+          </v-icon>
+        </v-btn>
+      </v-col>
+
+      </v-row>
+    </v-container>
+
 </template>
 
 <script>
-export default {
-  
+import axios from 'axios';
+
+  export default {
+
+    data: () => ({
+        arrayLocations: [],
+        numPage:1,
+    }),
+    created(){
+    this.getCharacter(this.numPage)
+  },
+  methods:{
+    getCharacter(num) {
+    const urlApiCharacter = `https://rickandmortyapi.com/api/location/?page=${num}`
+
+    axios
+      .get(urlApiCharacter)
+      .then(response =>
+        (this.arrayLocations = response.data.results)
+      )
+
+    this.scrollTop()
+  },
+    scrollTop(){
+    window.scrollTo(0, 0);
+  },
+  }
 }
 </script>
